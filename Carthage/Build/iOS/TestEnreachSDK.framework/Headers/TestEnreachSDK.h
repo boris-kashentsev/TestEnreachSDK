eba46780d67e12f5414eef95c4f4916084155c7b
boris-kashentsev <boris.kashentsev@conmio.com>
2017-07-17 19:07:33 +0300

    moved files around

--- Carthage/Build/iOS/TestEnreachSDK.framework/Headers/TestEnreachSDK.h
+++ Carthage/Build/iOS/TestEnreachSDK.framework/Headers/TestEnreachSDK.h
@@ -16,4 +16,9 @@ FOUNDATION_EXPORT const unsigned char TestEnreachSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TestEnreachSDK/PublicHeader.h>
 
-
+#import <TestEnreachSDK/Enreach.h>
+#import <TestEnreachSDK/AdInfo.h>
+#import <TestEnreachSDK/AdStatState.h>
+#import <TestEnreachSDK/Constants.h>
+#import <TestEnreachSDK/EnreachPaths.h>
+#import <TestEnreachSDK/CampaignResponse.h>
